b6ddce0195ed288582f5afbc3ce07958beeb1384
Karson Tang (TECHVIFY.D2) <karson.tang@techvify.com.vn>
2024-08-26 17:14:39 +0700

    Initial project

---
+++ api/src/main/java/vn/com/lol/nami/NamiApplication.java
@@ -0,0 +1,13 @@
+package vn.com.lol.nami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamiApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/vn/com/lol/nami/NamiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.lol.nami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
